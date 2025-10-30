6cbdba301d64d4b4d3c89b3b7fbac5259bf41af3
abhishektehriya <abhisheksharma09072004@gmail.com>
2025-10-30 12:06:04 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+package com.example.springdi;
+
+public class Student {
+    private Course course;
+
+    public Student(Course course) {
+        this.course = course;
+    }
+
+    public void showDetails() {
+        System.out.println("Student details:");
+        course.displayCourse();
+    }
+}
